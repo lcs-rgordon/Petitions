1a121d9a789807eb6e045dac675b48c074a2aeba
Russell Gordon <rgordon@lcs.on.ca>
2021-08-02 11:44:34 -0400

    Initial Commit

---
+++ Petitions/PetitionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PetitionsApp.swift
+//  Petitions
+//
+//  Created by Russell Gordon on 2021-08-02.
+//
+
+import SwiftUI
+
+@main
+struct PetitionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
